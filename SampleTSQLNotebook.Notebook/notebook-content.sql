aaee3aa8a48c77bc899579d13b2d33a4a7648bce
tomaszlibera <50926986+tomaszlibera@users.noreply.github.com>
2025-01-16 15:28:10 +0100

    Committing 3 items from workspace a718c418-7ea1-4a41-b2d7-defdd69ef1ac

---
+++ SampleTSQLNotebook.Notebook/notebook-content.sql
@@ -0,0 +1,41 @@
+-- Fabric notebook source
+
+-- METADATA ********************
+
+-- META {
+-- META   "kernel_info": {
+-- META     "name": "sqldatawarehouse"
+-- META   },
+-- META   "dependencies": {
+-- META     "warehouse": {
+-- META       "default_warehouse": "366ec5f0-595c-441d-9b28-fd5f38166ef8",
+-- META       "known_warehouses": [
+-- META         {
+-- META           "id": "366ec5f0-595c-441d-9b28-fd5f38166ef8",
+-- META           "type": "MountedWarehouse"
+-- META         }
+-- META       ]
+-- META     }
+-- META   }
+-- META }
+
+-- CELL ********************
+
+SELECT * FROM SalesLT.Product
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sql",
+-- META   "language_group": "sqldatawarehouse"
+-- META }
+
+-- CELL ********************
+
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sql",
+-- META   "language_group": "sqldatawarehouse"
+-- META }
